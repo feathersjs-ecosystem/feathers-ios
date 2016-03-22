358ee9aa66cadce3afb3348aa78f3b04e34768f6
Brendan Conron <conronb@gmail.com>
2016-03-21 22:24:50 -0400

    Created basic service interface

--- Pod/Classes/Core/Services/Service.swift
+++ Pod/Classes/Core/Services/Service.swift
@@ -8,19 +8,38 @@
 
 import Foundation
 
-/**
- Details the interface for a feathers service. Conformers may implement
- all or none of the methods as default implementations are provided via protocol
- extension.
- */
-public protocol Service {
+public class Service {
 
-    static func find()
-    static func get()
-    static func create()
-    static func remove()
-    static func update()
-    static func patch()
-    static func setup()
+}
 
+public protocol ServiceType {
+
+    func find<T, Error: ErrorType>(parameters: [String: AnyObject]?, block: (error: Error, result: [T]) -> ())
+    func get<T, Error: ErrorType>(id: String, parameters: [String: AnyObject]?, block: (error: Error, result: T))
+    func create<T, Error: ErrorType>(data: [String: AnyObject], parameters: [String: AnyObject]?, block: (error: Error, result: T))
+    func update<T, Error: ErrorType>(id: String, data: [String: AnyObject], parameters: [String: AnyObject]?, block: (error: Error, result: T))
+    func patch<T, Error: ErrorType>(id: String, data: [String: AnyObject], parameters: [String: AnyObject]?, block: (error: Error, result: T))
+    func remove<T, Error: ErrorType>(id: String, parameters: [String: AnyObject]?, block: (error: Error, result: T))
+    func setup(app: Feathers, path: String)
+
+    var service: Service { get }
+
+}
+
+public extension ServiceType {
+    func find<T, Error: ErrorType>(parameters: [String: AnyObject]?, block: (error: Error, result: [T]) -> ()) {}
+    func get<T, Error: ErrorType>(id: String, parameters: [String: AnyObject]?, block: (error: Error, result: T)) {}
+    func create<T, Error: ErrorType>(data: [String: AnyObject], parameters: [String: AnyObject]?, block: (error: Error, result: T)) {}
+    func update<T, Error: ErrorType>(id: String, data: [String: AnyObject], parameters: [String: AnyObject]?, block: (error: Error, result: T)) {}
+    func patch<T, Error: ErrorType>(id: String, data: [String: AnyObject], parameters: [String: AnyObject]?, block: (error: Error, result: T)) {}
+    func remove<T, Error: ErrorType>(id: String, parameters: [String: AnyObject]?, block: (error: Error, result: T)) {}
+    func setup(app: Feathers, path: String) {}
+}
+
+
+
+extension Service: ServiceType {
+    public var service: Service {
+        return self
+    }
 }
