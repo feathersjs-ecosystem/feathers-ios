d6f6c99888166c099971a1533be42075c5b73983
Brendan Conron <conronb@gmail.com>
2016-03-21 20:37:32 -0400

    added simple stub protocol and methods for services

--- Pod/Classes/Core/Feathers.swift
+++ Pod/Classes/Core/Feathers.swift
@@ -10,8 +10,23 @@ import UIKit
 
 public class Feathers {
     static let singleton = Feathers()
+    private init() {}
+
+    private var providers: [Provider] = []
+    private var services: [String: Service] = [:]
+}
+
+
+public extension Feathers {
+
+    public final func configure(provider: Provider) {
+        self.providers.append(provider)
+    }
+
 }
 
 public extension Feathers {
-    public final func use()
+    public final func use(location: String, service: Service) {
+        self.services[location] = service
+    }
 }
