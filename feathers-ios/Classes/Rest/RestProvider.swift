f4d5082acb8a9b7f5cff33467ca8229e3311d598
Brendan Conron <conronb@gmail.com>
2016-05-26 14:17:42 -0400

    added empty rest provider

---
+++ feathers-ios/Classes/Rest/RestProvider.swift
@@ -0,0 +1,41 @@
+//
+//  RestProvider.swift
+//  Pods
+//
+//  Created by Brendan Conron on 5/25/16.
+//
+//
+
+import Foundation
+import Result
+
+public class RestProvider: Provider {
+
+    required public init(baseURL: NSURL) {
+
+    }
+
+    public func find(parameters: [String: AnyObject], callback: (Result<[AnyObject], ServiceError>) -> ()) {
+
+    }
+
+    public func get(id: String, parameters: [String: AnyObject], callback: (Result<AnyObject?, ServiceError>) -> ()) {
+
+    }
+
+    public func create(data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>) -> ()) {
+
+    }
+
+    public func update(id: String, data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>) -> ()) {
+
+    }
+
+    public func patch(id: String, data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>) -> ()) {
+
+    }
+
+    public func remove(id: String, parameters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>) -> ()) {
+
+    }
+}
