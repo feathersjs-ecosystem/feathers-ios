7e3c782efab22ba48aabe3268f015610379a1991
Brendan Conron <conronb@gmail.com>
2016-06-08 02:53:07 -0400

    added default service implementation

--- feathers-ios/Classes/Core/Service.swift
+++ feathers-ios/Classes/Core/Service.swift
@@ -14,45 +14,51 @@ public enum ServiceError: ErrorType {
     case MappingObjectFailed(AnyObject)
 }
 
-public class Service {
-
-    internal var provider: Provider?
-
-    public func find(parameters: [String: AnyObject], callback: (Result<[AnyObject], ServiceError>) -> ()) {
-        self.provider?.find(parameters, callback: callback)
-    }
-
-    public func get(id: String, paramters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>)) {
-
-    }
-
-    public func create(data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>)) {
-
-    }
-
-    public func update(id: String, data: [String: AnyObject], paramters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>)) {
-
-    }
-
-    public func patch(id: String, data: [String: AnyObject], paramters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>)) {
-
-    }
-
-    public func remove(id: String, parameters: [String: AnyObject], callback: (Result<AnyObject, ServiceError>)) {
-
-    }
-
-}
-
 public protocol ServiceType {
 
     associatedtype Model
 
     func find(parameters: [String: AnyObject], callback: (Result<[Model], ServiceError>) -> ())
-    func get(id: String, paramters: [String: AnyObject], callback: (Result<Model, ServiceError>))
-    func create(data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>))
-    func update(id: String, data: [String: AnyObject], paramters: [String: AnyObject], callback: (Result<Model, ServiceError>))
-    func patch(id: String, data: [String: AnyObject], paramters: [String: AnyObject], callback: (Result<Model, ServiceError>))
-    func remove(id: String, parameters: [String: AnyObject], callback: (Result<Model, ServiceError>))
+    func get(id: String, parameters: [String: AnyObject], callback: (Result<Model?, ServiceError>) -> ())
+    func create(data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ())
+    func update(id: String, data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ())
+    func patch(id: String, data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ())
+    func remove(id: String, parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ())
+
+}
+
+
+public class Service {
+
+    public typealias Model = AnyObject
+
+    internal var provider: Provider?
+
+}
+
+extension Service: ServiceType {
+    public func find(parameters: [String: AnyObject], callback: (Result<[Model], ServiceError>) -> ()) {
+        self.provider?.find(parameters, callback: callback)
+    }
+
+    public func get(id: String, parameters: [String: AnyObject], callback: (Result<Model?, ServiceError>) -> ()) {
+        self.provider?.get(id, parameters: parameters, callback: callback)
+    }
+
+    public func create(data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ()) {
+        self.provider?.create(data, parameters: parameters, callback: callback)
+    }
+
+    public func update(id: String, data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ()) {
+        self.provider?.update(id, data: data, parameters: parameters, callback: callback)
+    }
+
+    public func patch(id: String, data: [String: AnyObject], parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ()) {
+        self.provider?.patch(id, data: data, parameters: parameters, callback: callback)
+    }
+
+    public func remove(id: String, parameters: [String: AnyObject], callback: (Result<Model, ServiceError>) -> ()) {
+        self.provider?.remove(id, parameters: parameters, callback: callback)
+    }
 
 }
